d4a6102f7f134a76714acfac8f1cd0db190744f5
Lazar Antonijevic <lazar.antonijevic123@gmail.com>
2026-06-24 23:52:25 +0200

    Updated coingecko.py
    - Centralized the base URL
    
    - Added the support for the CoinGecko API key
    
    - Removed unused example function

--- ingestion/sources/coingecko.py
+++ ingestion/sources/coingecko.py
@@ -1,31 +1,23 @@
 import asyncio
 import json
+import os
 
 import httpx
 
+COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY")
+BASE_URL = "https://api.coingecko.com/api/v3"
 
-# Sample data fetch from CoinGecko and print in console
-async def fetch_sample_data() -> None:
-    url = "https://api.coingecko.com/api/v3/coins/markets"
-    params = {
-        "vs_currency": "usd",
-        "order": "market_cap_desc",
-        "per_page": "5",
-        "page": "1",
-    }
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
-        response.raise_for_status()
-        data = response.json()
-
-        print("RAW JSON:")
-        print(json.dumps(data, indent=2))
+# Get needed header for API authentication
+def get_header() -> dict:
+    if not COINGECKO_API_KEY:
+        return {}
+    return {"x-cg-demo-api-key": COINGECKO_API_KEY}
 
 
 # Fetch top n coin IDs (by market cap)
 async def fetch_top_n_coin_ids(n: int) -> list[str]:
-    url = "https://api.coingecko.com/api/v3/coins/markets"
+    url = f"{BASE_URL}/coins/markets"
     params = {
         "vs_currency": "usd",
         "order": "market_cap_desc",
@@ -34,7 +26,7 @@ async def fetch_top_n_coin_ids(n: int) -> list[str]:
     }
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
+        response = await client.get(url, params=params, headers=get_header())
         response.raise_for_status()
         data = response.json()
 
@@ -43,20 +35,19 @@ async def fetch_top_n_coin_ids(n: int) -> list[str]:
 
 # Fetch current market data for a list of coins
 async def fetch_current_market_data(coin_ids: list[str]) -> list[dict]:
-    url = "https://api.coingecko.com/api/v3/coins/markets"
+    url = f"{BASE_URL}/coins/markets"
     params = {
         "vs_currency": "usd",
         "ids": ",".join(coin_ids),
     }
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
+        response = await client.get(url, params=params, headers=get_header())
         response.raise_for_status()
         return response.json()
 
 
 if __name__ == "__main__":
-    # asyncio.run(fetch_sample_data())
     ids = asyncio.run(fetch_top_n_coin_ids(5))
     res = asyncio.run(fetch_current_market_data(ids))
     print(json.dumps(res, indent=2))
