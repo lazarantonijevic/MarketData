ec3dc11f234b312a607110183d2c238b2650c6c4
Lazar Antonijevic <lazar.antonijevic123@gmail.com>
2026-07-14 00:47:18 +0200

    Create the stg_prices dbt model
    - stg_prices is an initial staging model with cleaned and typecasted data from the ingestion layer
    
    - materialized as a view
    
    - created the relevant schema and sources

---
+++ transform/models/staging/stg_prices.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from {{ source('raw', 'raw_prices') }}
+),
+
+cleaned as (
+    select
+        coin_id::varchar                            as coin_id,
+        symbol::varchar                             as symbol,
+        name::varchar                               as name,
+        price_usd::double                           as price_usd,
+        market_cap::double                          as market_cap,
+        vol_24h::double                             as vol_24h,
+        price_change_24h_pct::double                as price_change_24h_pct,
+        high_24h::double                            as high_24h,
+        low_24h::double                             as low_24h,
+        ingested_at::timestamptz                    as ingested_at,
+        date_trunc('day', ingested_at::timestamptz) as date_day
+    from source
+    where price_usd > 0
+        and price_usd is not null
+        and coin_id is not null
+)
+
+select * from cleaned
